15b43631ad1dcda1fd498d66cb906590b2b40e94
Susana Munguia <susana56@g.ucla.edu>
2018-04-22 16:47:33 -0700

    Initial Commit

---
+++ Homework/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Homework
+//
+//  Created by Susana Munguia on 4/22/18.
+//  Copyright © 2018 Susana Munguia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
